831677d2c763f5e324abc4af90f1d2c1d0983cb3
Haoming <nh.haoming@gmail.com>
2022-09-06 01:10:23 +0900

    会議時以外にカスタムオーバーレイ表示ボタンを押した時に発生するnull参照の解消

--- TheOtherRoles/CustomOverlays.cs
+++ TheOtherRoles/CustomOverlays.cs
@@ -537,11 +537,13 @@ namespace TheOtherRoles
         {
             public static void Postfix(KeyboardJoystick __instance)
             {
-                if (Input.GetKeyDown(KeyCode.H) && AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started && !DestroyableSingleton<ChatController>.Instance.IsOpen)
+                ChatController cc = DestroyableSingleton<ChatController>.Instance;
+                bool isOpen = cc != null && cc.IsOpen;
+                if (Input.GetKeyDown(KeyCode.H) && AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started && !isOpen)
                 {
                     toggleInfoOverlay();
                 }
-                else if (Input.GetKeyDown(KeyCode.I) && AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started && !DestroyableSingleton<ChatController>.Instance.IsOpen)
+                else if (Input.GetKeyDown(KeyCode.I) && AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started && !isOpen)
                 {
                     toggleRoleOverlay();
                 }
